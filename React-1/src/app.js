8e7ac03bb78cf868b7beea0f7b43c82e9f0ec20f
Md Danish <danishreza733@gmail.com>
2023-10-11 16:28:13 +0530

    Assignment updated

---
+++ React-1/src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Display from './display';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My React App</h1>
+      <Display /> {}
+    </div>
+  );
+}
+
+export default App;
+
---
+++ React-1/src/display.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function display() {
+  return (
+    <div>
+      <h1>Hello, I'm Md Danish and using react</h1>
+    </div>
+  );
+}
+
+export default display;
+
+// #this file name is display.js 
